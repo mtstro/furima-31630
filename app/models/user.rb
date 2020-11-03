63c17ef1b9cac78d7b418f0f0f0fdf2aacb33ed0
mtstro <superior.1210.c@gmail.com>
2020-11-03 15:57:13 +0900

    新規登録バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password,      format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英数字を使用して６文字以上で入力してください' }
+
   validates :nickname,       presence: true
-  validates :fam_name,       presence: true
-  validates :fir_name,       presence: true
-  validates :fam_name_read,  presence: true
-  validates :fir_name_read,  presence: true
+  validates :fam_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'ひらがな、カタカナ、漢字を使用して入力してください' }
+  validates :fir_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'ひらがな、カタカナ、漢字を使用して入力してください' }
+  validates :fam_name_read,  presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'カタカナを使用して入力してください' }
+  validates :fir_name_read,  presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'カタカナを使用して入力してください' }
   validates :birthday,       presence: true
 
 end
