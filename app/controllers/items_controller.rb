8a7a59e8a6ca3cba81faaabb0e912684d033daf8
mtstro <superior.1210.c@gmail.com>
2020-11-06 12:05:38 +0900

    商品情報編集機能　edit,update追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC").includes(:user)
@@ -19,11 +21,34 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :about, :category_id, :status_id, :ship_cost_id, :region_id, :ship_date_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    if current_user.id != item.user_id
+      redirect_to action: :index
+    end
+  end
+
 end
